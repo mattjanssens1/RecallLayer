8c3e365b0d0d4ae6edd22d2eeb7e52ee08a0283f
mattjanssens1 <matt.jans@yahoo.com>
2026-03-29 23:26:58 -0400

    Add unit test for xlarge benchmark runner

---
+++ tests/unit/test_xlarge_runner.py
@@ -0,0 +1,7 @@
+from turboquant_db.benchmark.xlarge_runner import run_xlarge_benchmark
+
+
+def test_xlarge_benchmark_runner_returns_rows() -> None:
+    artifacts = run_xlarge_benchmark(root_prefix=".test_xlarge_benchmark")
+    assert artifacts.rows
+    assert artifacts.fixture_name.startswith("xlarge_synthetic")
