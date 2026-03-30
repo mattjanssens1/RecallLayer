3dfb8bedeef3993b4e3a34e150313f6c0556d22f
mattjanssens1 <matt.jans@yahoo.com>
2026-03-29 21:16:50 -0400

    Add runtime test for metadata filter evaluator

---
+++ tests/unit/test_filters_runtime.py
@@ -0,0 +1,7 @@
+from turboquant_db.filter_eval import build_filter_fn
+
+
+def test_filters_runtime_predicate() -> None:
+    predicate = build_filter_fn({"region": {"eq": "us"}})
+    assert predicate({"region": "us"}) is True
+    assert predicate({"region": "ca"}) is False
