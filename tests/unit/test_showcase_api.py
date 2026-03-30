b0ffb31f672a3fbfd90362594e1744a9bf3da0cb
mattjanssens1 <matt.jans@yahoo.com>
2026-03-29 21:27:45 -0400

    Add unit test for showcase API health endpoint

---
+++ tests/unit/test_showcase_api.py
@@ -0,0 +1,6 @@
+from turboquant_db.api.showcase_server import create_showcase_app
+
+
+def test_showcase_api_app_factory_exists() -> None:
+    app = create_showcase_app()
+    assert app.title == "TurboQuant Native Vector Database Showcase"
