6b523a231c92fe193da4af08c4ae7c4fdf63be3f
mattjanssens1 <matt.jans@yahoo.com>
2026-03-29 23:30:03 -0400

    Switch to observed_plus showcase app

--- src/turboquant_db/api/app_observed.py
+++ src/turboquant_db/api/app_observed.py
@@ -1,5 +1,6 @@
-from turboquant_db.api.showcase_server_observed import app, create_observed_showcase_app
+from turboquant_db.api.showcase_server_observed_plus import app, create_observed_plus_showcase_app
 
-create_app = create_observed_showcase_app
+create_app = create_observed_plus_showcase_app
 
 __all__ = ["app", "create_app"]
+
